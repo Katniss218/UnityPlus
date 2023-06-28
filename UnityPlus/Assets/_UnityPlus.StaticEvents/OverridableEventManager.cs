e15bd7ad9c760bc8c5c50d189cf0832674ff51cb
Katniss218 <52105856+Katniss218@users.noreply.github.com>
2023-06-28 21:54:04 +0200

    fixes to overridable events

--- UnityPlus/Assets/_UnityPlus.StaticEvents/OverridableEventManager.cs
+++ UnityPlus/Assets/_UnityPlus.StaticEvents/OverridableEventManager.cs
@@ -6,74 +6,11 @@ using UnityEngine;
 namespace UnityPlus.StaticEvents
 {
     /// <summary>
-    /// Manages static game events.
+    /// Manages overridable game events.
     /// </summary>
-    public class StaticEventManager
+    public class OverridableEventManager
     {
-        class Event
-        {
-            public Event( string id )
-            {
-                this._id = id;
-            }
-
-            string _id;
-
-            Dictionary<string, Action<object>> _listeners = new Dictionary<string, Action<object>>();
-            HashSet<string> _blacklist = new HashSet<string>();
-
-            public bool TryAddListener( OverridableEventListener<Action<object>> listener )
-            {
-                if( _listeners.ContainsKey( listener.id ) )
-                {
-                    return false;
-                }
-
-                // Allows a mod to block itself (!), but I don't think that's a problem.
-                foreach( string blockedId in listener.blacklist )
-                {
-                    _blacklist.Add( blockedId );
-                }
-
-                // Remove listners that are on the new blacklist.
-                foreach( string blockedId in listener.blacklist )
-                {
-                    if( _listeners.Remove( blockedId ) )
-                    {
-                        Debug.Log( $"StaticEventManager: `{_id}`: Listener `{blockedId}` was blocked by `{listener.id}`." );
-                    }
-                }
-
-                if( _blacklist.Contains( listener.id ) )
-                {
-                    Debug.Log( $"StaticEventManager: `{_id}`: Listener `{listener.id}` is blocked." );
-                    return false;
-                }
-
-                _listeners.Add( listener.id, listener.func );
-                return true;
-            }
-
-            public bool TryInvoke( object obj )
-            {
-                foreach( var listener in _listeners.Values )
-                {
-                    try
-                    {
-                        listener( obj );
-                    }
-                    catch( Exception ex )
-                    {
-                        Debug.LogException( ex );
-                    }
-                }
-                return true;
-            }
-        }
-
-        // You can't remove listeners. This is by design.
-
-        Dictionary<string, Event> _events;
+        Dictionary<string, OverridableEvent> _events = new Dictionary<string, OverridableEvent>();
 
         /// <summary>
         /// Tries to create an event with the given ID.
@@ -91,7 +28,7 @@ namespace UnityPlus.StaticEvents
                 return false;
             }
 
-            _events.Add( eventId, new Event( eventId ) );
+            _events.Add( eventId, new OverridableEvent( eventId ) );
             return true;
         }
 
@@ -121,13 +58,17 @@ namespace UnityPlus.StaticEvents
                 throw new ArgumentNullException( nameof( eventId ), $"Event ID can't be null." );
             }
 
-            if( _events.TryGetValue( eventId, out Event @event ) )
+            if( _events.TryGetValue( eventId, out OverridableEvent @event ) )
             {
                 return @event.TryAddListener( listener );
             }
 
             return false; // unknown event ID.
         }
+
+        // You can't remove listeners.
+        // This is because removing them would necessitate caching the entire invocation list, regardless of what is blocked,
+        //   and then unblocking those that would no longer be blocked by the removed listener.
 
         /// <summary>
         /// Safely invokes each non-blocked listener of a specific event one-by-one.
@@ -142,7 +83,7 @@ namespace UnityPlus.StaticEvents
                 throw new ArgumentNullException( nameof( eventId ), $"Event ID can't be null." );
             }
 
-            if( _events.TryGetValue( eventId, out Event @event ) )
+            if( _events.TryGetValue( eventId, out OverridableEvent @event ) )
             {
                 return @event.TryInvoke( obj );
             }
