88f5613ec18a7bfdba5093fe48f042b535bb8942
Katniss218 <52105856+Katniss218@users.noreply.github.com>
2024-04-12 21:25:30 +0200

    fixed recttransformdragger sometimes dragging despite having child elements blocking the UI raycast

--- UnityPlus/Assets/_UnityPlus.UILib/RectTransformDragger.cs
+++ UnityPlus/Assets/_UnityPlus.UILib/RectTransformDragger.cs
@@ -8,73 +8,69 @@ using UnityEngine.EventSystems;
 
 namespace UnityPlus.UILib
 {
-	/// <summary>
-	/// Enables a <see cref="RectTransform"/> to be dragged around by the mouse.
-	/// </summary>
-	public class RectTransformDragger : EventTrigger
-	{
-		[field: SerializeField]
-		public RectTransform UITransform { get; set; }
+    /// <summary>
+    /// Enables a <see cref="RectTransform"/> to be dragged around by the mouse.
+    /// </summary>
+    public class RectTransformDragger : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+    {
+        [field: SerializeField]
+        public RectTransform UITransform { get; set; }
 
-		[field: SerializeField]
-		public PointerEventData.InputButton MouseButton { get; set; } = PointerEventData.InputButton.Left;
+        [field: SerializeField]
+        public PointerEventData.InputButton MouseButton { get; set; } = PointerEventData.InputButton.Left;
 
-		bool _isDragging = false;
-		Vector2 _cursorOffset = Vector2.zero;
+        bool _isDragging = false;
+        Vector2 _cursorOffset = Vector2.zero;
 
-		public Action OnBeginDragging { get; set; }
-		public Action OnDragging { get; set; }
-		public Action OnEndDragging { get; set; }
+        public Action OnBeginDragging { get; set; }
+        public Action OnDragging { get; set; }
+        public Action OnEndDragging { get; set; }
 
-		void Update()
-		{
-			if( UITransform == null )
-			{
-				return;
-			}
+        public void OnBeginDrag( PointerEventData eventData )
+        {
+            if( UITransform == null )
+            {
+                return;
+            }
 
-			if( _isDragging )
-			{
-				UITransform.position = new Vector2( Input.mousePosition.x, Input.mousePosition.y ) + _cursorOffset;
-				OnDragging?.Invoke();
-			}
-		}
+            if( eventData.button != MouseButton )
+            {
+                return;
+            }
 
-		public override void OnPointerDown( PointerEventData eventData )
-		{
-			if( UITransform == null )
-			{
-				return;
-			}
+            _cursorOffset = new Vector2( UITransform.position.x, UITransform.position.y ) - new Vector2( Input.mousePosition.x, Input.mousePosition.y );
+            _isDragging = true;
+            OnBeginDragging?.Invoke();
+        }
 
-			if( eventData.button != MouseButton )
-			{
-				return;
-			}
+        public void OnDrag( PointerEventData eventData )
+        {
+            if( UITransform == null )
+            {
+                return;
+            }
 
-			_cursorOffset = new Vector2( UITransform.position.x, UITransform.position.y ) - new Vector2( Input.mousePosition.x, Input.mousePosition.y );
-			_isDragging = true;
-			OnBeginDragging?.Invoke();
+            if( _isDragging )
+            {
+                UITransform.position = new Vector2( Input.mousePosition.x, Input.mousePosition.y ) + _cursorOffset;
+                OnDragging?.Invoke();
+            }
+        }
 
-			base.OnPointerDown( eventData );
-		}
+        public void OnEndDrag( PointerEventData eventData )
+        {
+            if( UITransform == null )
+            {
+                return;
+            }
 
-		public override void OnPointerUp( PointerEventData eventData )
-		{
-			if( UITransform == null )
-			{
-				return;
-			}
+            if( eventData.button != MouseButton )
+            {
+                return;
+            }
 
-			if( eventData.button != MouseButton )
-			{
-				return;
-			}
-
-			_isDragging = false;
-			OnEndDragging?.Invoke();
-
-			base.OnPointerUp( eventData );
-		}
-	}
+            _isDragging = false;
+            OnEndDragging?.Invoke();
+        }
+    }
 }
