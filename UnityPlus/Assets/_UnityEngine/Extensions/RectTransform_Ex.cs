9ad48b2cc0dd1913b4693efedb3be7305b4d3a60
Katniss218 <52105856+Katniss218@users.noreply.github.com>
2024-04-20 12:35:19 +0200

    UIScrollBar and UIScrollView now work in tandem properly. viewport size depends on the scrollbars, and scrollbars are locked to either top, bottom, left, or right side and have only their size available to be set. Added placeholder verticalfittosizelayoutdriver until new layout can be made properly

--- UnityPlus/Assets/_UnityEngine/Extensions/RectTransform_Ex.cs
+++ UnityPlus/Assets/_UnityEngine/Extensions/RectTransform_Ex.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace UnityEngine
 {
-	public static class RectTransform_Ex
-	{
+    public static class RectTransform_Ex
+    {
         /// <summary>
         /// True if the rect transform is set to fill the width of its parent.
         /// </summary>
@@ -22,67 +22,59 @@ namespace UnityEngine
         /// Gets the center point of the rect transform.
         /// </summary>
         public static Vector2 GetLocalCenter( this RectTransform rectTransform )
-		{
-			return rectTransform.rect.center;
-		}
+        {
+            return rectTransform.rect.center;
+        }
 
-		/// <summary>
-		/// Transforms a position in the local space of one rect transform to the local space of another.
-		/// </summary>
-		public static Vector2 TransformPointTo( this RectTransform from, Vector2 position, RectTransform to )
-		{
-			Vector2 canvasSpacePos = from.TransformPoint( position ); // this transforms from/to pivot space.
-			return to.InverseTransformPoint( canvasSpacePos );
-		}
+        /// <summary>
+        /// Transforms a point from the local space of <paramref name="from"/>, to the local space of <paramref name="to"/>.
+        /// </summary>
+        public static Vector2 TransformPointTo( this RectTransform from, Vector2 position, RectTransform to )
+        {
+            Vector2 canvasSpacePos = from.TransformPoint( position ); // this transforms from/to pivot space.
+            return to.InverseTransformPoint( canvasSpacePos );
+        }
 
-		/// <summary>
-		/// Calculates the actual size of a rect transform in canvas space.
-		/// </summary>
-		public static Vector2 GetActualSize( this RectTransform rt )
-		{
-			Stack<(Vector2 size, Vector2 anchor)> hierarchyDeltas = new Stack<(Vector2, Vector2)>();
+        /// <summary>
+        /// Gets the actual size of a rect transform in canvas space.
+        /// </summary>
+        public static Vector2 GetActualSize( this RectTransform rectTransform )
+        {
+            return rectTransform.rect.size;
+        }
 
-			hierarchyDeltas.Push( (rt.sizeDelta, rt.anchorMax - rt.anchorMin) );
+        /// <summary>
+        /// Sets the screen space position to the value provided by using <see cref="Camera.WorldToScreenPoint(Vector3)"/>.
+        /// </summary>
+        public static void SetScreenPosition( this RectTransform rt, Vector3 screenSpacePosition, bool hideWhenBehindCamera = true )
+        {
+            if( screenSpacePosition.z < 0 && hideWhenBehindCamera )
+                screenSpacePosition = new Vector3( float.MaxValue, 0.0f, float.MaxValue );
+            else
+                screenSpacePosition.z = 0.0f; // reset depth.
+            rt.position = screenSpacePosition;
+        }
 
-			// Figure out which objects contribute to the actual size and reverse their order (to 'parent then child').
-			while( rt.parent != null )
-			{
-				rt = (RectTransform)rt.parent;
-				if( rt.anchorMax != rt.anchorMin ) // if the anchors are equal, then the actual size is always equal to sizeDelta.
-				{
-					hierarchyDeltas.Push( (rt.sizeDelta, rt.anchorMax - rt.anchorMin) );
-				}
-			}
+        /// <summary>
+        /// Sets the screen space position to the world space position viewed by the given camera.
+        /// </summary>
+        public static void SetScreenPosition( this RectTransform rt, Camera camera, Vector3 worldSpacePosition, bool hideWhenBehindCamera = true )
+        {
+            Vector3 screenSpacePosition = camera.WorldToScreenPoint( worldSpacePosition, camera.stereoActiveEye );
+            SetScreenPosition( rt, screenSpacePosition, hideWhenBehindCamera );
+        }
 
-			// Calculate the actual size.
-			Vector2 currentSize = Vector2.zero;
-			foreach( var delta in hierarchyDeltas )
-			{
-				currentSize = (currentSize * delta.anchor) + delta.size;
-			}
+        public static void SetVerticalMargins( this RectTransform rectTransform, float top, float bottom )
+        {
+            rectTransform.sizeDelta = new Vector2( rectTransform.sizeDelta.x, -(top + bottom) );
+            rectTransform.anchoredPosition = new Vector2( rectTransform.anchoredPosition.x, (bottom - top) / 2f );
+        }
 
-			return currentSize;
-		}
+        public static void SetHorizontalMargins( this RectTransform rectTransform, float left, float right )
+        {
+            rectTransform.sizeDelta = new Vector2( -(left + right), rectTransform.sizeDelta.y );
+            rectTransform.anchoredPosition = new Vector2( (left - right) / 2f, rectTransform.anchoredPosition.y );
+        }
 
-		/// <summary>
-		/// Sets the screen space position to the value provided by using <see cref="Camera.WorldToScreenPoint(Vector3)"/>.
-		/// </summary>
-		public static void SetScreenPosition( this RectTransform rt, Vector3 screenSpacePosition, bool hideWhenBehindCamera = true )
-		{
-			if( screenSpacePosition.z < 0 && hideWhenBehindCamera )
-				screenSpacePosition = new Vector3( float.MaxValue, 0.0f, float.MaxValue );
-			else
-				screenSpacePosition.z = 0.0f; // reset depth.
-			rt.position = screenSpacePosition;
-		}
-
-		/// <summary>
-		/// Sets the screen space position to the world space position viewed by the given camera.
-		/// </summary>
-		public static void SetScreenPosition( this RectTransform rt, Camera camera, Vector3 worldSpacePosition, bool hideWhenBehindCamera = true )
-		{
-			Vector3 screenSpacePosition = camera.WorldToScreenPoint( worldSpacePosition, camera.stereoActiveEye );
-			SetScreenPosition( rt, screenSpacePosition, hideWhenBehindCamera );
-		}
-	}
+    }
 }
