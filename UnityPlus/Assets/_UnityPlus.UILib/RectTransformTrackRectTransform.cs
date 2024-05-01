10d8a1cd5743a7a0ea6bb38a85ddbffdd6cead37
Katniss218 <52105856+Katniss218@users.noreply.github.com>
2024-05-01 12:43:36 +0200

    more enhancements to input elements

--- UnityPlus/Assets/_UnityPlus.UILib/RectTransformTrackRectTransform.cs
+++ UnityPlus/Assets/_UnityPlus.UILib/RectTransformTrackRectTransform.cs
@@ -11,13 +11,25 @@ namespace UnityPlus.UILib
         /// </summary>
         public RectTransform Target { get; set; }
 
+        /// <summary>
+        /// Gets or sets the additional offset to apply to this transform.
+        /// </summary>
         public Vector2 Offset { get; set; }
 
+        /// <summary>
+        /// Gets or sets the corner of the target transform that will be aligned with this transform.
+        /// </summary>
         public Vector2 TargetCorner { get; set; }
+
+        /// <summary>
+        /// Gets or sets the corner of this transform that will be aligned with the target.
+        /// </summary>
         public Vector2 SelfCorner { get; set; }
 
         void LateUpdate()
         {
+            // Put the two corners 'on top of each other', and translate self by the offset.
+
             RectTransform self = (RectTransform)transform;
             Rect targetRect = Target.rect;
 
