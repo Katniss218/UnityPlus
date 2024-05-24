6dab9949d9edee471c96accfc7bee4c582bc54d9
Katniss218 <52105856+Katniss218@users.noreply.github.com>
2024-05-24 21:01:30 +0200

    added file logger and changed console logger into UI logger. Added applicationutils class to group utility methods related to the unity executable itself (base path for now)

--- UnityPlus/Assets/_UnityPlus.Logging/LogUIOutput.cs
+++ UnityPlus/Assets/_UnityPlus.Logging/LogUIOutput.cs
@@ -3,37 +3,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace UnityPlus.Console
+namespace UnityPlus.Logging
 {
     /// <summary>
-    /// Specifies that a given UI element will be used as log output (see <see cref="_output"/>). <br />
+    /// Enables forwarding of log messages (see <see cref="Output"/>). <br />
     /// Should probably be placed in a scene that doesn't get unloaded.
     /// </summary>
     [DisallowMultipleComponent]
-    public class LogTextOutput : MonoBehaviour
+    public sealed class LogUIOutput : MonoBehaviour
     {
-        static string LOG_COLOR_INFO = "#dddddd";
-        static string LOG_COLOR_WARN = "#dddd55";
-        static string LOG_COLOR_ERROR = "#dd5555";
+        private const string LOG_COLOR_INFO = "#dddddd";
+        private const string LOG_COLOR_WARN = "#dddd55";
+        private const string LOG_COLOR_ERROR = "#dd5555";
 
-        static string LOG_COLOR_EXCEPTION = "#dd5555";
-        static string LOG_COLOR_EXCEPTION_STACK = "#c55555";
+        private const string LOG_COLOR_EXCEPTION = "#dd5555";
+        private const string LOG_COLOR_EXCEPTION_STACK = "#c55555";
 
-        [SerializeField]
-        private GameObject _consoleUIElement = null;
+        [field: SerializeField]
+        private TMPro.TextMeshProUGUI Output { get; set; } = null;
 
-        [SerializeField]
-        private TMPro.TextMeshProUGUI _output = null;
+        private void Initialize()
+        {
+            if( Output == null )
+            {
+                throw new InvalidOperationException( $"Can't initialize {nameof( LogUIOutput )} - Output UI element is not specified." );
+            }
 
-        [SerializeField]
-        private string _defaultText = "Console:\n\n";
+            if( !Output.richText )
+            {
+                Debug.LogWarning( $"The output UI element ({Output}) isn't set to Rich Rext, setting to Rich Text now." );
+                Output.richText = true;
+            }
+
+            Output.text = Output.text ?? ""; // This is required to fix glitch requiring reenabling the gameObject after adding some text to the output (if it's set to blank).
+            Output.gameObject.SetActive( false );
+        }
 
         /// <summary>
         /// Prints a string out to the console.
         /// </summary>
         public void Print( string message )
         {
-            _output.text += message;
+            if( Output == null )
+                Initialize();
+
+            Output.text += message;
         }
 
         /// <summary>
@@ -41,14 +55,17 @@ namespace UnityPlus.Console
         /// </summary>
         public void PrintLine( string message )
         {
-            _output.text += $"{message}\n";
+            if( Output == null )
+                Initialize();
+
+            Output.text += $"{message}\n";
         }
 
         private void HandleLog( string message, string stackTrace, LogType logType )
         {
             switch( logType )
             {
-                case LogType.Log:
+                default:
                     PrintLine( $"<color={LOG_COLOR_INFO}>[{DateTime.Now.ToLongTimeString()}](___) - {message}</color>" );
                     break;
 
@@ -58,31 +75,14 @@ namespace UnityPlus.Console
 
                 case LogType.Error:
                     PrintLine( $"<color={LOG_COLOR_ERROR}>[{DateTime.Now.ToLongTimeString()}](ERR) - {message}</color>" );
-                    _consoleUIElement.SetActive( true );
+                    Output.gameObject.SetActive( true );
                     break;
 
                 case LogType.Exception:
                     PrintLine( $"<color={LOG_COLOR_EXCEPTION}>[{DateTime.Now.ToLongTimeString()}](EXC) - {message}</color>\n  at\n<color={LOG_COLOR_EXCEPTION_STACK}>" + stackTrace + "</color>" );
-                    _consoleUIElement.SetActive( true );
+                    Output.gameObject.SetActive( true );
                     break;
             }
-        }
-
-        void Awake()
-        {
-            if( _consoleUIElement == null || _consoleUIElement == this.transform )
-            {
-                throw new InvalidOperationException( $"Can't initialize {nameof( LogTextOutput )} - {nameof( _consoleUIElement )} must be non-null and not equal to the gameobject that this component is attached to." );
-            }
-
-            if( !_output.richText )
-            {
-                Debug.LogWarning( $"The console ({_output}) isn't set to Rich Rext, setting to Rich Text now." );
-                _output.richText = true;
-            }
-
-            _output.text = _defaultText; // This is required to fix glitch requiring reenabling the gameObject after adding some text to the output (if it's set to blank).
-            _consoleUIElement.SetActive( false );
         }
 
         void OnEnable()
