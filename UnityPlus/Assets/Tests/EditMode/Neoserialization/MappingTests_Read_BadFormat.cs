e6b680bc3e5aa6acc96b1c68e9437c2a0848cda5
Katniss218 <52105856+Katniss218@users.noreply.github.com>
2025-05-10 11:12:14 +0200

    found a problem where a failed deserialization still creates gameobjects that are disabled and out of hierarchy, but still exist, allowing the things that use them to fail silently in weird ways

--- UnityPlus/Assets/Tests/EditMode/Neoserialization/MappingTests_Read_BadFormat.cs
+++ UnityPlus/Assets/Tests/EditMode/Neoserialization/MappingTests_Read_BadFormat.cs
@@ -8,24 +8,51 @@ namespace Neoserialization
     public class MappingTests_Read_BadFormat
     {
         [Test]
-        public void Reading___BadType___DeserializedAsDefault()
+        public void Reading___MissingTypeHeader___()
         {
 #warning TODO - Try to read a type with/without header and the data has the opposite. Deserialization should result in failures until the member is eventually left at what it was.
 
+        }
+        
+        [Test]
+        public void Reading___MissingIDHeader___CreatedButNotAddedToTheRefStore()
+        {
+            var initialValue = new SerializedObject()
+            {
+                { "$type", "Neoserialization.OwningClass, EditMode, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null" },
+                //{ "$id", "1cfbb788-5602-4b03-bc62-39e1ecb0e6b6" },
+                { "ref_member", (SerializedPrimitive)null }
+            };
+
+            var refMap = new BidirectionalReferenceStore();
+
+            var obj = SerializationUnit.Deserialize<OwningClass>( ObjectContext.Value, initialValue, refMap );
+            bool isInRefmap = refMap.TryGetID( obj, out var guid );
+
+            Assert.That( obj, Is.EqualTo( new OwningClass() ) );
+            Assert.That( isInRefmap, Is.False );
+        }
+
+        [Test]
+        public void Reading___ArrayInsteadOfObject___()
+        {
+#warning TODO - maybe handle this with the 'errors', add an error, map it to a hierarchy of guids that can be followed to get to the gameobject(s) to delete them.
             // Arrange
             var initialValue = new SerializedObject()
             {
-                { "$type", "UnityEngine.GameObjectNonExistent, UnityEngine.CoreModule, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null" },
+                { "$type", "Neoserialization.OwningClass, EditMode, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null" },
                 { "$id", "1cfbb788-5602-4b03-bc62-39e1ecb0e6b6" },
-                { "value", (SerializedPrimitive)true }
+                { "ref_member", new SerializedArray() {
+                } }
             };
             var refMap = new BidirectionalReferenceStore();
 
             // Act
-            var data = SerializationUnit.Deserialize<object>( ObjectContext.Value, initialValue, refMap );
+            var obj = SerializationUnit.Deserialize<OwningClass>( ObjectContext.Value, initialValue, refMap );
 
             // Assert
-            Assert.That( data, Is.EqualTo( default( object ) ) );
+            //Assert.Throws<Exception>( () => SerializationUnit.Deserialize<OwningClass>( ObjectContext.Value, initialValue, refMap ) );
+            Assert.That( obj, Is.EqualTo( new OwningClass() ) );
         }
     }
 }
