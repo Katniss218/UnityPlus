ad72fb7926748cfbffed617b1a9eb7d1fe94b1a3
Katniss218 <52105856+Katniss218@users.noreply.github.com>
2026-04-06 17:38:04 +0200

    Added an abilityy to diff 2 SerializedData trees

---
+++ UnityPlus/Assets/_UnityPlus.Serialization/DescriptorProviders/UnityPlusPrimitiveDescriptors.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityPlus.Serialization.Descriptors;
+
+namespace UnityPlus.Serialization.DescriptorProviders
+{
+    public static class UnityPlusPrimitiveDescriptors
+    {
+        // --- VECTORS ---
+
+        [MapsInheritingFrom( typeof( Vector3Dbl ) )]
+        public static IDescriptor Vector3Descriptor() => new PrimitiveConfigurableDescriptor<Vector3>(
+            ( target, wrapper, ctx ) =>
+                wrapper.Data = new SerializedArray { (SerializedPrimitive)target.x, (SerializedPrimitive)target.y, (SerializedPrimitive)target.z },
+            ( data, ctx ) =>
+                (data is SerializedArray arr && arr.Count >= 3) ? new Vector3( (float)arr[0], (float)arr[1], (float)arr[2] ) : default
+        );
+
+        // --- QUATERNION ---
+
+        [MapsInheritingFrom( typeof( QuaternionDbl ) )]
+        public static IDescriptor QuaternionDescriptor() => new PrimitiveConfigurableDescriptor<Quaternion>(
+            ( target, wrapper, ctx ) =>
+                wrapper.Data = new SerializedArray { (SerializedPrimitive)target.x, (SerializedPrimitive)target.y, (SerializedPrimitive)target.z, (SerializedPrimitive)target.w },
+            ( data, ctx ) =>
+                (data is SerializedArray arr && arr.Count >= 4) ? new Quaternion( (float)arr[0], (float)arr[1], (float)arr[2], (float)arr[3] ) : default
+        );
+
+        [MapsInheritingFrom( typeof( FloatInterpolator ) )]
+        public static IDescriptor FloatInterpolatorMapping()
+        {
+            return new MemberwiseDescriptor<FloatInterpolator>();
+        }
+
+        [MapsInheritingFrom( typeof( MappingCurve<>.Keyframe ) )]
+        public static IDescriptor KeyframeMapping<T>()
+        {
+            return new MemberwiseDescriptor<MappingCurve<T>.Keyframe>()
+                .WithMember( "t", o => o.Time )
+                .WithMember( "v", o => o.Value )
+                .WithMember( "v_in", o => o.InTangent )
+                .WithMember( "v_out", o => o.OutTangent );
+        }
+
+        [MapsInheritingFrom( typeof( MappingCurve<> ) )]
+        public static IDescriptor AnimationCurveMapping<T>()
+        {
+            return new MemberwiseDescriptor<MappingCurve<T>>()
+                .WithReadonlyMember( "interpolator", o => o.Interpolator )
+                .WithFactory<IMappingCurveInterpolator<T>>( ( interpolator ) => new MappingCurve<T>( interpolator ), "interpolator" )
+                .WithMember( "keys", o => o.Keyframes );
+        }
+
+        [MapsInheritingFrom( typeof( LinearMappingCurve<> ) )]
+        public static IDescriptor LinearMappingCurveMapping<T>()
+        {
+            return new MemberwiseDescriptor<LinearMappingCurve<T>>()
+                .WithReadonlyMember( "interpolator", o => o.Interpolator )
+                .WithFactory<IMappingCurveInterpolator<T>>( ( interpolator ) => new LinearMappingCurve<T>( interpolator ), "interpolator" );
+        }
+    }
+}
